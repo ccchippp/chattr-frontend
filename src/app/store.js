fe16bbe38b782c728d46fd65ea25d6b870d74e56
ccchippp <clempke90@gmail.com>
2021-04-19 10:23:34 -0400

    retry

--- src/app/store.js
+++ src/app/store.js
@@ -6,8 +6,7 @@ export default configureStore({
     reducer: {
         user: userReducer,
         app: appReducer,
-        
     }
-}+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
+}
+// +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
