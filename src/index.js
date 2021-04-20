bbace5d032128251f6261028e8862518952559da
ccchippp <clempke90@gmail.com>
2021-04-20 10:29:37 -0400

    fixed bug

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux'
 import { Provider } from 'react-redux';
+import store from './app/store'
 
 // const store = createStore(reducer)
 
 ReactDOM.render(
-  <Provider >
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
